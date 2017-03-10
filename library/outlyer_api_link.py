ac4026d8d134eb4c7eb72d28e43bc75d3a60ab34
Daniel Siechniewicz / nullDowntime Ltd <daniel@nulldowntime.com>
2017-03-10 04:31:37 +0000

    * library/outlyer_api_link.py - simplified, removed updates as they didn't make much sense

--- library/outlyer_api_link.py
+++ library/outlyer_api_link.py
@@ -35,76 +35,24 @@ def check_link_exists(module):
         api_url,
         headers=headers
     )
-    # {
-    #     "id": "5899cefc9f4a6e7b49840d7b",
-    #     "org": "587f7a8cf48d0d3a7479bcd7",
-    #     "plugin": "java",
-    #     "tags": [
-    #         "java"
-    #     ]
-    # },
 
-    out = {'found': False, 'complete': False, 'error': False, 'data': None}
+    out = {'found': False, 'error': False, 'data': None}
 
     if resp.status_code == 200:
 
         for link in resp.json():
-            if link['plugin'] == module.params['plugin']:
+            if ( link['plugin'] == module.params['plugin']
+                 and set(link['tags']) == set(module.params['tags'])
+                 ):
                 out['found'] = True
                 out['data'] = link
-                ###module.fail_json(msg=('link: %s' % link))
                 break
-
-        if out['data']:
-            c = 0
-            for m in module.params['tags']:
-                if m in link['tags']:
-                    c += 1
-            if c == len(module.params['tags']):
-                out['complete'] = True
 
     elif resp.status_code not in [200, 404]:
         resp.raise_for_status()
         out['error'] = True
 
     return out
-
-def update_link(module, le):
-    tags = set()
-
-    for t in module.params['tags']:
-        tags.add(t)
-
-    for t in le['data']['tags']:
-        tags.add(t)
-
-    module.params['tags'] = list(tags)
-
-    try:
-        delete_link(module, le)
-    except requests.exceptions.RequestException, err_str:
-        module.fail_json(msg='Request to delete link failed', reason=err_str)
-
-    try:
-        create_link(module)
-    except requests.exceptions.RequestException, err_str:
-        module.fail_json(msg='Request to create link failed', reason=err_str)
-
-    return True
-
-
-def recreate_link(module, le):
-    try:
-        delete_link(module, le)
-    except requests.exceptions.RequestException, err_str:
-        module.fail_json(msg='Request to delete link failed', reason=err_str)
-
-    try:
-        create_link(module)
-    except requests.exceptions.RequestException, err_str:
-        module.fail_json(msg='Request to create link failed', reason=err_str)
-
-    return True
 
 
 def create_link(module):
@@ -145,7 +93,6 @@ def main():
         apikey=dict(required=True, no_log=True),
         plugin=dict(required=True),
         tags=dict(required=True, type='list'),
-        replace_tags=dict(required=False, default=False, type='bool'),
         state=dict(required=False, default='present', choices=['present', 'absent'])
     )
 
@@ -159,30 +106,12 @@ def main():
     except requests.exceptions.RequestException, err_str:
         module.fail_json(msg='Request to check link existence failed', reason=err_str)
 
-    ##module.fail_json(msg=('le: %s' % le))
-
     changed = False
     msg = []
 
     if module.params['state'] == 'present':
-        if le['found'] and le['complete']:
+        if le['found']:
             msg.append('link already exists and contains the specified tag(s)')
-        elif le['found']:
-            # UPDATE link - found but not complete, does not contain the desired tag(s)
-            if module.params['replace_tags']:
-                rl = recreate_link(module, le)
-                if rl:
-                    msg.append('link recreated')
-                    changed = True
-                else:
-                    module.fail_json(msg='link update failed')
-            else:
-                ul = update_link(module, le)
-                if ul:
-                    msg.append('link updated')
-                    changed = True
-                else:
-                    module.fail_json(msg='link update failed')
         else:
             try:
                 create_link(module)
@@ -206,6 +135,5 @@ def main():
 
 # import module snippets
 from ansible.module_utils.basic import *
-#from ansible.module_utils.api import *
 
 main()
